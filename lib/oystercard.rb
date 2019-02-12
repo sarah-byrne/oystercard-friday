c722492f921451b1c111192cf542bd342761e6f4
Maria Vanaale <mvanaale@gmail.com>
2019-02-12 14:51:07 +0000

    step 9

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,6 +1,7 @@
 class Oystercard
 
   MAX_BALANCE = 90
+  MIN_BALANCE = 1
 
   attr_reader :balance
 
@@ -24,6 +25,7 @@ class Oystercard
   end
 
   def touch_in
+    raise "The money is not enough in card." if balance <= MIN_BALANCE
     @in_journey = true
   end
 
