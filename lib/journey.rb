30696eb36349e3bcd66e4126cb64b1a65097c951
Lima Vadakara <lima.vadakara@gmail.com>
2019-02-13 19:13:16 +0000

    Add Journey class that starts and finishes a journey and calculates the right fare. It also indicates if it has finished

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,7 +1,25 @@
 class Journey
-  attr_reader :start, :finished
-  def initialize
-    @start = true
-    @finished = true 
+  attr_reader :state, :starting_station, :ending_station
+
+  def initialize(starting_station = nil)
+    @state = "started"
+    @starting_station = starting_station
   end
+
+  def finish(ending_station = nil)
+    @ending_station = ending_station
+    @state = "finished"
+  end
+
+  def complete?
+    return true if @state == "finished"
+    false
+  end
+
+  def fare
+    return 6 if @starting_station == nil
+    return 6 if @ending_station == nil
+    1
+  end
+
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -1,14 +1,63 @@
 require 'journey'
 
 describe Journey do
-  let (:oystercard) {double(Oystercard.new, :balance => 10)}
+  let (:oystercard) {double("Oystercard", :balance => 10)}
+  let (:starting_station) {double("Station", :name => "Oxford Station", :zone => 1)}
 
-  it 'returns if the journey has started' do
-    journey = Journey.new
-    expect(journey.start).to eq true
+  it 'starts when created' do
+    journey = Journey.new(starting_station)
+    expect(journey.state).to eq "started"
   end
-  it "returns the journey is complete" do
+
+  it 'gets created with a starting station if passed in' do
+    journey = Journey.new(starting_station)
+    expect(journey.starting_station).to equal starting_station
+  end
+
+
+  it 'finishes when instructed to finish' do
+    journey = Journey.new(starting_station)
+    journey.finish(starting_station)
+    expect(journey.state).to eq "finished"
+  end
+
+  it 'sets the ending station to the input station when instructed to finish' do
+    journey = Journey.new(starting_station)
+    journey.finish(starting_station)
+    expect(journey.ending_station).to equal starting_station
+  end
+
+  it 'sets the ending station to nil if exit ending station not passed in when instructed to finish' do
+    journey = Journey.new(starting_station)
+    journey.finish
+    expect(journey.ending_station).to equal nil
+  end
+
+  it 'gets created with a nil starting station if not passed in' do
     journey = Journey.new
-    expect(journey.finished).to eq true
+    expect(journey.starting_station).to equal nil
+  end
+
+  it 'indicates if it is not complete' do
+    journey = Journey.new(starting_station)
+    expect(journey.complete?).to eq false
+  end
+
+  it 'returns the right fare for itself' do
+    journey = Journey.new(starting_station)
+    journey.finish(starting_station)
+    expect(journey.fare).to eq 1
+  end
+
+  it 'returns penalty fare of 6 for itself if entry station is not passed' do
+    journey = Journey.new
+    journey.finish(starting_station)
+    expect(journey.fare).to eq 6
+  end
+
+  it 'returns penalty fare of 6 for itself if exit station is not passed' do
+    journey = Journey.new(starting_station)
+    journey.finish
+    expect(journey.fare).to eq 6
   end
 end
